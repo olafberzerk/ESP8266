a5bea51e25dbf9b697a100364b5ffe4234821570
Brady Herder <bradyherder@hotmail.com>
2016-02-08 19:20:31 -0800

    Signed-off-by: Brady Herder <bradyherder@hotmail.com>

---
+++ .ino.cpp
@@ -0,0 +1,11 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2016-02-08 19:20:04
+
+#include "Arduino.h"
+void setup() ;
+void loop() ;
+
+#include "ESP8266.ino"
+
